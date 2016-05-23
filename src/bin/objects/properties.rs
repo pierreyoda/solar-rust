eaa53537e269b5e3c2673f41662eb0d2ac1a3dce
pierreyoda <pierreyoda@users.noreply.github.com>
2016-05-23 17:46:12 +0200

    solar_rustlib : improved ObjectRegister struct, properties can now be immutable (get_[type]_mut will always return None)

--- src/bin/objects/properties.rs
+++ src/bin/objects/properties.rs
@@ -14,6 +14,7 @@ pub enum ObjectPropertyValue {
 pub struct ObjectProperty {
     value: ObjectPropertyValue,
     description: String,
+    mutable: bool,
 }
 
 /// Each 'GameObject' owns such a register for easier storage and access of
@@ -42,27 +43,43 @@ macro_rules! register_get_mut {
         pub fn $getter_name<S: Into<String>>(&mut self, key: S) -> Option<&mut $enum_type> {
             let key_string = key.into();
             if !self.properties.contains_key(&key_string) {
-                let value: $enum_type = Default::default();
-                self.properties.insert(key_string.clone(), ObjectProperty {
-                    value: $enum_value(value),
-                    description: "auto-generated on demand by ObjectRegister's mutable getter".into(),
-                });
+                self.add_property(key_string.clone(),
+                                  $enum_value(Default::default()),
+                                  "auto-generated on demand by ObjectRegister's mutable getter".into());
             }
             self.properties.get_mut(&key_string).and_then(|property|
-                match property.value {
-                    $enum_value(ref mut v) => Some(v),
-                    _ => None,
-                })
+                if property.mutable {
+                    match property.value {
+                        $enum_value(ref mut v) => Some(v),
+                        _ => None,
+                    }
+                } else {
+                    None
+            })
         }
     )
 }
 
 impl ObjectRegister {
+    pub fn new() -> ObjectRegister {
+        Default::default()
+    }
+
+    pub fn add_constant<S: Into<String>>(&mut self, key: S, value: ObjectPropertyValue, desc: S) {
+        self.properties.insert(key.into(),
+                               ObjectProperty {
+                                   value: value,
+                                   description: desc.into(),
+                                   mutable: false,
+                               });
+    }
+
     pub fn add_property<S: Into<String>>(&mut self, key: S, value: ObjectPropertyValue, desc: S) {
         self.properties.insert(key.into(),
                                ObjectProperty {
                                    value: value,
                                    description: desc.into(),
+                                   mutable: true,
                                });
     }
 
@@ -84,23 +101,32 @@ impl ObjectRegister {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use super::ObjectPropertyValue::*;
 
     #[test]
-    fn test_object_register() {
-        use super::ObjectPropertyValue::*;
-
+    fn test_object_register_properties() {
         let mut register: ObjectRegister = Default::default();
 
         assert_eq!(register.get_int("test_int"), None);
         register.add_property("test_int", Integer(12), "Just a simple test variable.");
-        register.get_int_mut("test_int").map(|mut i| *i += 30);
+        register.get_int_mut("test_int").map(|i| *i += 30);
         assert_eq!(register.get_int("test_int"), Some(&42));
 
         assert_eq!(register.get_float("test_float"), None);
-        register.get_float_mut("test_float").map(|mut f| *f = 3.14);
+        register.get_float_mut("test_float").map(|f| *f = 3.14);
         assert_eq!(register.get_float("test_float"), Some(&3.14));
         let description = "Just a simple float variable".to_string();
         register.set_description("test_float".to_string(), description.clone());
         assert_eq!(register.get_description("test_float"), Some(&description));
+    }
+
+    #[test]
+    fn test_object_register_constants() {
+        let mut register = ObjectRegister::new();
+
+        assert_eq!(register.get_int("int_constant"), None);
+        register.add_constant("int_constant", Integer(3), "Just a simple test constant.");
+        register.get_int_mut("int_constant").map(|i| *i *= 2);
+        assert_eq!(register.get_int("int_constant"), Some(&3));
     }
 }
